67ac08d2bd83f01e2ad02dbf19665577571509e6
Matthew Morrison <M.Morrison@albynschool.org>
2025-03-10 09:55:26 +0000

    hihi

---
+++ select2.py
@@ -0,0 +1,12 @@
+username = input("Please enter your username: ")
+password = input("Please enter your password: ")
+if username == "ian" and password == "letmein":
+    print("Logon accepted")
+else: 
+    print("Password incorrect")
+
+#username=IAN, password=letmein = Password incorrect
+
+#username=ian, password=LETMEIN = Password incorrect
+
+#u  sername=ian, password=letmein = Logon accepted
