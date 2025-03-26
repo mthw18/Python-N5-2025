1ada4e72fe1f6f0cab7e7f3c7f469c11aca6b6c9
Matthew Morrison <M.Morrison@albynschool.org>
2025-03-26 15:30:55 +0000

    hihi

--- 309/309temptask.py
+++ 309/309temptask.py
@@ -1,6 +1,7 @@
-for index in range (0,5):
-    temperature = input("Enter a number between -20 and +50")
-    if temperature < -20 or temperature > 50:
-        print("Enter a valid temperature")
-    if temperature > -20 or temperature < 50:
-        print("Accepted temperature")
+temperature = []
+for index in range(0,5):
+    temp = input("Enter a daily temperature between -20 and +50")
+    while temperature < -20 or temperature > 50:
+        print("These are invalid temperatures")
+        temp = int(input("Please re-enter a valid temperature"))
+temperature.append(temp)
